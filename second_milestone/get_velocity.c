b49fad49a4b82512c0668cacd58b146a266635ad
shehab <shehabthelegand@gmail.com>
2021-06-13 21:19:24 +0200

    Create get_velocity.c

---
+++ second_milestone/get_velocity.c
@@ -0,0 +1,20 @@
+// #include <stdio.h>
+// #include <time.h>
+// int main()
+
+// {
+// clock_t begin = clock();
+/*
+    * main code here to get the distance   
+    */
+// clock_t end = clock();
+// double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+// double velocity;
+// double distance = 3; //what ever the calculated distance would be
+// velocity = distance / time_spent;
+
+// printf("\n%fl", time_spent);
+// printf("\n%fl", velocity);
+
+//     return 0;
+// }
